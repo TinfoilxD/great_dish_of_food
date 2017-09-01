b6176d26629866d1f842b6871cdaf8b02c785bbb
Patrick <patrick1jack@gmail.com>
2017-08-31 21:20:41 -0400

    added order classes

---
+++ gdf/src/main/java/com/revature/gspj/gdf/controller/OrderController.java
@@ -0,0 +1,11 @@
+package com.revature.gspj.gdf.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrderController {
+	
+	
+
+		
+}
---
+++ gdf/src/main/java/com/revature/gspj/gdf/service/OrderService.java
@@ -0,0 +1,5 @@
+package com.revature.gspj.gdf.service;
+
+public class OrderService {
+
+}
